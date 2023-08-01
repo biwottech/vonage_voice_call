57d613630b1e7b695204e68d362739719893f02c
Polycarp Masika <masikapolycarp@gmail.con>
2023-08-01 20:56:17 +0300

    call waiting example

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import * as XLSX from 'xlsx';
-import axios from 'axios';
+import * as XLSX from "xlsx";
+import axios from "axios";
 import "./App.css";
 
 function App() {
@@ -15,27 +15,55 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (excelFile != null) {
-      const workbook = XLSX.read(excelFile, { type: 'buffer' });
+      const workbook = XLSX.read(excelFile, { type: "buffer" });
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
       const data = XLSX.utils.sheet_to_json(sheet);
       setExceldata(data); // display data
-
-      axios.post(`http://localhost:5000/call/`, {
-        params: data
-      })
-        .then(apiResponse => {
-          const call = apiResponse.data
-          console.log(call);
-        })
+      makeCalls();
     }
+  };
+
+  async function getCallStatus(call_uuid) {
+    // hit call status endpoint
+    const response = await axios.get("/call-status", {
+      params: {
+        call_uuid,
+      },
+    });
+
+    return response.data;
   }
 
+  async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async function makeCalls() {
+    // we want to call each of the records in the excel sheet
+    // but we want to wait for each call to finish before we call the next one
+    for (let record of excelData) {
+      // call the record
+      // wait for it to finish
+      // then make the next call
+      await axios.post("/call", record);
+      let callStatus = await getCallStatus(record.call_uuid);
+      while (callStatus.status !== "completed") {
+        // wait for 3 seconds
+        await sleep(3000);
+        callStatus = await getCallStatus(record.call_uuid);
+      }
+    }
+  }
 
   // onchange event
   const handleFile = (e) => {
     let selectedFile = e.target.files[0];
-    let fileType = ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'text/csv'];
+    let fileType = [
+      "application/vnd.ms-excel",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "text/csv",
+    ];
     if (selectedFile) {
       if (selectedFile.type) {
         if (fileType.includes(selectedFile.type)) {
@@ -46,77 +74,84 @@ function App() {
           reader.readAsArrayBuffer(selectedFile);
           reader.onload = (e) => {
             setExcelFile(e.target.result);
-          }
+          };
         } else {
           setExcelFile(null);
-          setTypeError('Please select valid excel sheet');
+          setTypeError("Please select valid excel sheet");
         }
       }
     } else {
       console.log("Please select your file");
     }
-  }
-
-
+  };
 
   return (
     <div className="App">
-      <div className='container'>
-        <div className='row'>
-          <div className='col-sm-12 col-md-12 col-lg-12'>
-            <div className='card mt-30'>
-              <div className='card-header'>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-12">
+            <div className="card mt-30">
+              <div className="card-header">
                 <h3>Upload & View Excel Sheets</h3>
               </div>
-              <div className='card-body'>
-                <div className='row'>
-                  <div className='col-sm-12 col-md-12 col-lg-12'>
-                    <form className="row gx-3 gy-2 align-items-center" onSubmit={handleSubmit} encType="multipart/form-data">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-sm-12 col-md-12 col-lg-12">
+                    <form
+                      className="row gx-3 gy-2 align-items-center"
+                      onSubmit={handleSubmit}
+                      encType="multipart/form-data"
+                    >
                       <div className="col-sm-3">
-                        <label className="visually-hidden" htmlFor="specificSizeInputGroupUsername">Upload Excel Sheet</label>
+                        <label className="visually-hidden" htmlFor="specificSizeInputGroupUsername">
+                          Upload Excel Sheet
+                        </label>
                         <div className="input-group">
                           <div className="input-group-text">@</div>
-                          <input type="file" id='file' className="form-control" required onChange={handleFile} />
+                          <input type="file" id="file" className="form-control" required onChange={handleFile} />
                         </div>
                       </div>
                       <div className="col-auto">
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary">
+                          Submit
+                        </button>
                       </div>
                       {
                         /* error */
                         typeError && (
-                          <div className='alert alert-danger' role='alert'>{typeError}</div>
+                          <div className="alert alert-danger" role="alert">
+                            {typeError}
+                          </div>
                         )
                       }
                     </form>
                   </div>
                 </div>
               </div>
-              <div className='card-body'>
+              <div className="card-body">
                 {/* view data  */}
-                <div className='view-data'>
+                <div className="view-data">
                   <h3>View Data</h3>
                   {excelData ? (
-                    <div className='table-responsive'>
+                    <div className="table-responsive">
                       <h3>Excel Sheet Data</h3>
-                      <table className='table table-bordered'>
+                      <table className="table table-bordered">
                         <thead>
                           <tr>
-                            {
-                              Object.keys(excelData[0]).map((key) => (
-                                <th key={key}>{key} </th>
-                              ))
-                            }
+                            {Object.keys(excelData[0]).map((key) => (
+                              <th key={key}>{key} </th>
+                            ))}
                           </tr>
                         </thead>
                         <tbody>
                           {excelData.map((individualExcelData, index) => (
                             <tr key={index}>
-                              {
-                                Object.keys(individualExcelData).map((key) => (
-                                  <td key={key}> {key}   {individualExcelData[key]}</td>
-                                ))
-                              }
+                              {Object.keys(individualExcelData).map((key) => (
+                                <td key={key}>
+                                  {" "}
+                                  {key} {individualExcelData[key]}
+                                </td>
+                              ))}
                             </tr>
                           ))}
                         </tbody>
@@ -131,7 +166,7 @@ function App() {
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -11,8 +11,7 @@ const fs = require("fs");
 var isBuffer = require("is-buffer");
 var header = require("waveheader");
 var file;
-const dataPath = './contacts/userContacts.json';
-
+const dataPath = "./contacts/userContacts.json";
 
 const VONAGE_NUMBER = process.env.VONAGE_NUMBER;
 const ANSWER_URL = process.env.ANSWER_WEBHOOK_URL;
@@ -24,36 +23,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const vonage = new Vonage(
-    {
-        apiKey: process.env.VONAGE_API_KEY,
-        apiSecret: process.env.VONAGE_API_SECRET,
-        applicationId: process.env.VONAGE_APPLICATION_ID,
-        privateKey: process.env.VONAGE_PRIVATE_KEY_PATH,
-    },
-    { debug: true }
+  {
+    apiKey: process.env.VONAGE_API_KEY,
+    apiSecret: process.env.VONAGE_API_SECRET,
+    applicationId: process.env.VONAGE_APPLICATION_ID,
+    privateKey: process.env.VONAGE_PRIVATE_KEY_PATH,
+  },
+  { debug: true }
 );
 
 // initiate the call
 async function makeCall(user) {
-    let to = user.phone;
-    return vonage.voice.createOutboundCall(
-        {
-            to: [{ type: "phone", number: to }],
-            from: { type: "phone", number: VONAGE_NUMBER },
-            answer_url: [ANSWER_URL],
-            event_url: [EVENT_URL],
-        },
-        (err, response) => {
-            if (err) {
-                console.error("Error making outbound call:", err);
-            } else {
-                console.log("Outbound call response:", response);
-            }
-        }
-    );
+  let to = user.phone;
+  return vonage.voice.createOutboundCall(
+    {
+      to: [{ type: "phone", number: to }],
+      from: { type: "phone", number: VONAGE_NUMBER },
+      answer_url: [ANSWER_URL],
+      event_url: [EVENT_URL],
+    },
+    (err, response) => {
+      if (err) {
+        console.error("Error making outbound call:", err);
+      } else {
+        console.log("Outbound call response:", response);
+      }
+    }
+  );
 }
-
-
 
 //Serve a Main Page
 // app.get("/", function (req, res) {
@@ -61,140 +58,163 @@ async function makeCall(user) {
 // });
 
 const saveAccountData = (data) => {
-    const stringifyData = JSON.stringify(data)
-    fs.writeFileSync(dataPath, stringifyData)
-}
+  const stringifyData = JSON.stringify(data);
+  fs.writeFileSync(dataPath, stringifyData);
+};
 const getAccountData = () => {
-    const jsonData = fs.readFileSync(dataPath)
-    return JSON.parse(jsonData)
-}
+  const jsonData = fs.readFileSync(dataPath);
+  return JSON.parse(jsonData);
+};
 
+const callStatuses = {};
 
+app.get("/call-status", (req, res) => {
+  const call_uuid = req.query.call_uuid;
+  const status = callStatuses[call_uuid];
+  res.status(200).send(status);
+});
+
+app.post("call", (req, res) => {
+  // call the number
+  // then save the call status
+  // something like this:
+  callStatuses[req.body.call_uuid] = {
+    status: "ringing",
+  };
+});
 
 app.get("/", (req, res) => {
-    // let call_data = req.body.params;
-    const call_data = [
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-        { name: 'Evans Koech', phone: 254727143163, email: 'biwottech@gmail.com', status:'' },
-    ];
+  // let call_data = req.body.params;
+  const call_data = [
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+    { name: "Evans Koech", phone: 254727143163, email: "biwottech@gmail.com", status: "" },
+  ];
 
+  call_data.map((call) => {
+    console.log(call);
+    var existAccounts = getAccountData();
+    saveAccountData(existAccounts);
+    let caller_uuid = makeCall(call);
+    existAccounts[caller_uuid] = call;
+    let status = existAccounts[caller_uuid].status;
 
-
-    call_data.map(call => {
-        console.log(call)
-        var existAccounts = getAccountData();
-        saveAccountData(existAccounts);
-        let caller_uuid = makeCall(call);
-        existAccounts[caller_uuid] = call;
-        let status = existAccounts[caller_uuid].status;
-        
-        if(status == true){
-            // continue;
-        }else{
-            // break;
-        }
-    });
-    return res.status(200).send("Message delivered");
+    if (status == true) {
+      // continue;
+    } else {
+      // break;
+    }
+  });
+  return res.status(200).send("Message delivered");
 });
 
 const record_event_logs = (event) => {
-    var existAccounts = getAccountData();
-    fs.readFile(dataPath, 'utf8', (err, data) => {
-        const accountId = event.UUID;
-        existAccounts[accountId] = {
-            status:event.status
-        }
-    });
+  var existAccounts = getAccountData();
+  fs.readFile(dataPath, "utf8", (err, data) => {
+    const accountId = event.UUID;
+    existAccounts[accountId] = {
+      status: event.status,
+    };
+  });
 };
 
 app
-    .get("/answer", (req, res) => {
-        //Serve the NCCO on the /ncco answer URL
-        // answer event
-        let nccoResponse = [
-            {
-                action: "connect",
-                from: "NexmoTest",
-                endpoint: [
-                    {
-                        type: "websocket",
-                        uri: `wss://${req.hostname}/socket`,
-                        "content-type": "audio/l16;rate=16000",
-                    },
-                ],
-            },
-        ];
-        res.status(200).json(nccoResponse);
-    })
-    .ws("/socket", (ws, req) => {
-        // Handle the Websocket
-        var rawarray = [];
-        console.log("Websocket Connected");
-        ws.on("message", function (msg) {
-            ws.send(msg);  // returning the voice message directly to the user  // user can hear his/her voice
-            if (isBuffer(msg)) {
-                rawarray.push(msg);
-            } else {
-                console.log(msg);
-            }
-        });
-
-        ws.on("close", function () {
-            console.log("Websocket Closed");
-            // record user conversation // end the call
-            const audioFilePath = path.join(__dirname, "recordings", "output.wav");
-            file = fs.createWriteStream(audioFilePath);
-            file.write(
-                header(((16000 * rawarray.length) / 50) * 2, {
-                    sampleRate: 16000,
-                    channels: 1,
-                    bitDepth: 16,
-                })
-            );
-            rawarray.forEach(function (data) {
-                file.write(data);
-            });
-        });
+  .get("/answer", (req, res) => {
+    //Serve the NCCO on the /ncco answer URL
+    // answer event
+    let nccoResponse = [
+      {
+        action: "connect",
+        from: "NexmoTest",
+        endpoint: [
+          {
+            type: "websocket",
+            uri: `wss://${req.hostname}/socket`,
+            "content-type": "audio/l16;rate=16000",
+          },
+        ],
+      },
+    ];
+    res.status(200).json(nccoResponse);
+  })
+  .ws("/socket", (ws, req) => {
+    // Handle the Websocket
+    var rawarray = [];
+    console.log("Websocket Connected");
+    ws.on("message", function (msg) {
+      ws.send(msg); // returning the voice message directly to the user  // user can hear his/her voice
+      if (isBuffer(msg)) {
+        rawarray.push(msg);
+      } else {
+        console.log(msg);
+      }
     });
+
+    ws.on("close", function () {
+      console.log("Websocket Closed");
+      // record user conversation // end the call
+      const audioFilePath = path.join(__dirname, "recordings", "output.wav");
+      file = fs.createWriteStream(audioFilePath);
+      file.write(
+        header(((16000 * rawarray.length) / 50) * 2, {
+          sampleRate: 16000,
+          channels: 1,
+          bitDepth: 16,
+        })
+      );
+      rawarray.forEach(function (data) {
+        file.write(data);
+      });
+    });
+  });
 
 //Log the Events
 app.post("/event", function (req, res) {
-    let status = req.body.status;
-    let conversation_uuid = req.body.conversation_uuid;
-    switch (status) {
-        case "ringing":
-            record_event_logs({ UUID:conversation_uuid, status:'ringing' });
-            break;
-        case "answered":
-            record_event_logs({ UUID:conversation_uuid, status:'answered' });
-            break;
-        case "complete":
-            record_event_logs({ UUID:conversation_uuid, status:'complete' });
-            break;
-        case 'canceled':
-            record_event_logs({ UUID:conversation_uuid, status:'canceled' });
-            break;
-        default:
-            break;
-    }
-    return res.status(204).send("");
+  let status = req.body.status;
+  let conversation_uuid = req.body.conversation_uuid;
+  switch (status) {
+    case "ringing":
+      record_event_logs({ UUID: conversation_uuid, status: "ringing" });
+      callStatuses[conversation_uuid] = {
+        status: "ringing",
+      };
+      break;
+    case "answered":
+      record_event_logs({ UUID: conversation_uuid, status: "answered" });
+      callStatuses[conversation_uuid] = {
+        status: "answered",
+      };
+      break;
+    case "complete":
+      record_event_logs({ UUID: conversation_uuid, status: "complete" });
+      callStatuses[conversation_uuid] = {
+        status: "complete",
+      };
+      break;
+    case "canceled":
+      record_event_logs({ UUID: conversation_uuid, status: "canceled" });
+      callStatuses[conversation_uuid] = {
+        status: "canceled",
+      };
+      break;
+    default:
+      break;
+  }
+  return res.status(204).send("");
 });
 
 // websocket connection
 expressWs.getWss().on("connection", function (ws) {
-    console.log("Websocket connection is open");
-    // console.log(ws);
+  console.log("Websocket connection is open");
+  // console.log(ws);
 });
 
-app.listen(PORT, () =>
-    console.log(`Listening on port http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`));
